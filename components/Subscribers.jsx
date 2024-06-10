4f05c5e1b794a339a562147a18b9be9de7b907d8
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-06-10 17:43:10 +0530

    added card component

--- components/Subscribers.jsx
+++ components/Subscribers.jsx
@@ -1,8 +1,29 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
 
 const Subscribers = () => {
-  return (
-    <div>Subscribers</div>
-  )
-}
+    return (
+        <div className="flex flex-col justify-center items-center my-4">
+            <Card>
+                <CardHeader>
+                    <CardTitle>Subscription Added</CardTitle>
+                    <CardDescription>Please check your email</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Wow!! Tapas Adhikary Subscribed.</p>
+                </CardContent>
+                <CardFooter>
+                    <p>Last Subscription at: {new Date().toTimeString()}</p>
+                </CardFooter>
+            </Card>
+        </div>
+    );
+};
 
-export default Subscribers
+export default Subscribers;
